21887d60682412f4e8c78dce99482b32732051dc
Vladiuse <vladiuse@gmail.com>
2021-08-19 19:52:44 +0300

    add static, dell scripts, old temps

---
+++ myapp/static/myapp/main.js
@@ -0,0 +1,36 @@
+    let counter = 2;
+    a = {'name-1': '',
+    'name-2': '',
+  }
+  function collectData(){
+    let form = document.getElementById('form').elements
+    for (key in a){
+      console.log(form[key].value)
+      a[key] = form[key].value
+    }
+  }
+    function onLoad(){
+      var mainDiv = document.getElementById('main')
+      mainDiv.innerHTML = ''
+      for (key in a) {
+        newInput = document.createElement('input')
+        newLabel = document.createElement('label')
+        newInput.name = key;
+        newInput.classList.add('form-control')
+        newInput.value = a[key];
+        newLabel.innerHTML = key;
+        newLabel.classList.add('form-label')
+        newInput.style.display = 'block';
+        mainDiv.appendChild(newLabel)
+        mainDiv.appendChild(newInput)
+      }
+    }
+    function toAdd(){
+      collectData();
+      counter += 1;
+      let newName = 'name-'+ counter;
+      a[newName] = '';
+      console.log(newName, a)
+      onLoad();
+    }
+    onLoad();
--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,22 @@
 import json
-from configparser import ConfigParser
-from .models import Inputs
 
-import psycopg2
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.urls import reverse
 
-
+from .models import Inputs
 
 
 def index(request):
     if request.method == 'POST':
         result = dict(request.POST)
         result.pop('csrfmiddlewaretoken')
-        # print(result)
         for k, v in result.items():
             result[k] = v[0]
-        # print(result)
         my_json = json.dumps(result)
         new_input = Inputs(data=my_json)
         new_input.save()
+        return HttpResponseRedirect(reverse('myapp:all_data'))
     return render(request, 'myapp/index.html')
 
 
@@ -29,4 +26,3 @@ def all_data(request):
         my_json = json.loads(str(data))
         my_dict['result'].append(my_json)
     return JsonResponse(my_dict)
-
